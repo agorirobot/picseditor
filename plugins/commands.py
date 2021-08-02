7f249554db2cefdc4035e728a3252829cf61ba85
Personal Robot <88285322+agorirobot@users.noreply.github.com>
2021-08-02 14:39:56 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -19,7 +19,7 @@ async def start(client: Client, message: Message):
                     [
                         InlineKeyboardButton(
                             "SOURCE CODE",
-                            url="https://github.com/TroJanzHEX/Image-Editor",
+                            url="https://github.com/agorirobot/picseditor",
                         )
                     ],
                 ]
@@ -71,7 +71,7 @@ async def about(client, message):
                     [
                         InlineKeyboardButton(
                             "SOURCE CODE",
-                            url="https://github.com/TroJanzHEX/Image-Editor",
+                            url="https://github.com/Agorirobot/Picseditor",
                         )
                     ],
                 ]
